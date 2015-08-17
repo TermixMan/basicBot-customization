7e9540803d64fb1b9b50c3e8b79beaf5ffb8b818
TermixMan <TermixMan@email.cz>
2015-08-17 12:25:45 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,10 +46,10 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
-      language: "english",
-      chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-      scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+      botName: "-MusicBot-",
+      language: "czech",
+      chatLink: "https://rawgit.com/TermixMan/basicBot/master/lang/en.json",
+      scriptLink: "https://rawgit.com/TermixMan/basicBot/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
@@ -95,9 +95,9 @@
       etaRestriction: false,
       welcome: true,
       opLink: null,
-      rulesLink: null,
+      rulesLink: http://bit.ly/1TLonuq,
       themeLink: null,
-      fbLink: null,
+      fbLink: http://on.fb.me/1WxLCMF,
       youtubeLink: null,
       website: null,
       intervalMessages: [],
@@ -105,13 +105,13 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/TermixMan/basicBot-customization/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/TermixMan/basicBot-customization/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/TermixMan/basicBot-customization/master/blacklists/BANNEDlist.json"
       }
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/TermixMan/basicBot/master/basicBot.js", extend);
 
 }).call(this);
