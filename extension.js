0b043c3efb5cb783d03c699feb908e60d0004aaf
TermixMan <TermixMan@email.cz>
2015-08-18 11:12:49 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,8 +48,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "-MusicBot-",
       language: "czech",
-      chatLink: "https://rawgit.com/TermixMan/basicBot/master/lang/en.json",
-      scriptLink: "https://rawgit.com/TermixMan/basicBot/master/basicBot.js",
+      chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/cs.json",
+      scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/TermixMan/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
 
 }).call(this);
